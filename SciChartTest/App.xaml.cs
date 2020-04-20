2490ccb11ce7eb8e5b40f31df2d72fe8a4a249c3
Jeremy Nelson <jeremy@biosensinginstrument.com>
2020-04-20 15:47:53 -0700

    Log unhandled exceptions to a file.

---
+++ SciChartTest/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace SciChartTest
+{
+    public partial class App
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+        }
+
+        private void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            File.AppendAllText("Error.log", ((Exception)args.ExceptionObject).ToString() + Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
