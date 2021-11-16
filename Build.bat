2de71f32c8806c21afbaca0b88bd55153d4ff9eb
Jeremy Nelson <jeremy@biosensinginstrument.com>
2021-11-16 13:04:23 -0700

    Add net6.0-windows to TargetFrameworks.

--- Build.bat
+++ Build.bat
@@ -11,3 +11,4 @@ call git clean -fdx
 call msbuild SciChartTest.sln /m /v:m /p:Configuration=Release /p:TargetFramework=net472 /restore
 dotnet publish -f netcoreapp3.1 -c Release
 dotnet publish -f net5.0-windows -c Release
+dotnet publish -f net6.0-windows -c Release
