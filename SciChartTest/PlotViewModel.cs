0050a20b7b5abe676fe4a4d88758f0d1df2e779a
Jeremy Nelson <jeremy@biosensinginstrument.com>
2022-01-26 20:20:16 -0700

    ColumnRenderableSeriesViewModel with log Y axis.

--- SciChartTest/PlotViewModel.cs
+++ SciChartTest/PlotViewModel.cs
@@ -1,66 +1,23 @@
 ﻿using Prism.Mvvm;
 using SciChart.Charting.Model.ChartSeries;
 using SciChart.Charting.Model.DataSeries;
-using SciChartTest;
-using System;
 using System.Collections.ObjectModel;
-using System.Threading;
 
 namespace ViewModel
 {
-    public class PlotViewModel : BindableBase, IDisposable
+    public class PlotViewModel : BindableBase
     {
         public ObservableCollection<IRenderableSeriesViewModel> RenderableSeries { get; } = new ObservableCollection<IRenderableSeriesViewModel>();
 
-        private readonly System.Timers.Timer timer = new(10.0);
-        private readonly XyDataSeries<double, double> series = new();
         public PlotViewModel()
         {
-            RenderableSeries.Add(new LineRenderableSeriesViewModel() { DataSeries = series });
-            timer.Elapsed += (x, y) => OnTimer();
-            timer.Start();
+            var series = new XyDataSeries<double, double>();
+            series.Append(0.0, 1e-8);
+            series.Append(1.0, 1e-7);
+            series.Append(2.0, 1e-6);
+            series.Append(3.0, 1e-5);
+            series.Append(4.0, 1e-4);
+            RenderableSeries.Add(new ColumnRenderableSeriesViewModel() { DataSeries = series, ZeroLineY = 1e-7 });
         }
-
-        private double x;
-        private readonly Random random = new();
-        private int processingTimer;
-        private void OnTimer()
-        {
-            if (Interlocked.Exchange(ref processingTimer, 1) == 0)
-            {
-                try
-                {
-                    series.Append(x++, random.NextDouble());
-                }
-                catch (Exception e)
-                {
-                    Log.Append("Exception thrown from Append:\n{0}", e);
-                }
-                finally
-                {
-                    Interlocked.Exchange(ref processingTimer, 0);
-                }
-            }
-        }
-
-        #region Dispose
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private bool disposed;
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposed)
-                return;
-
-            if (disposing)
-                timer.Dispose();
-
-            disposed = true;
-        }
-        #endregion
     }
 }
